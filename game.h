f01c7bde6e9b454f98d022ae32b44af1daaceb79
russell Hofvendahl <rhofvendahl@gmail.com>
2019-10-16 17:52:03 -0700

    fill out game.h

--- game.h
+++ game.h
@@ -1,11 +1,24 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include "color.h"
+#include "cell.h"
 
-class game
+class Game
 {
 public:
-    game();
+    Game();
+    QList<QList<Cell>> board;
+    void update();
+    void insert(Cell insertCell, QList<QList<bool>> shape, QColor color);
+    void clear();
+    void random(); // originally passed in list of colors
+
+private:
+    Cell inRangeCell(int  row, int column);
+    QList<Cell> getNeighbors(cell Cell);
+    QColor averageColor(QList<Cell> cells);
+
 };
 
 #endif // GAME_H
