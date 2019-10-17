07a455d9cc31b0cfd5d998979d7bdc845bd2d1b7
russell Hofvendahl <rhofvendahl@gmail.com>
2019-10-16 21:31:29 -0700

    fill out Library class

--- library.cpp
+++ library.cpp
@@ -1,6 +1,19 @@
 #include "library.h"
 
+#include <QList>
+
 Library::Library()
 {
+    glider = QList<QList<bool>>({QList<bool>({false, true, false}),
+                                 QList<bool>({false, false, true}),
+                                 QList<bool>({true, true, true})});
 
+    toad = QList<QList<bool>>({QList<bool>({false, true, true, true}),
+                                 QList<bool>({true, true, true, false})});
+
+    beehive = QList<QList<bool>>({QList<bool>({false, true, true, false}),
+                                 QList<bool>({true, false, false, true}),
+                                 QList<bool>({false, true, true, false})});
+
+    shapes = QList<QList<QList<bool>>>({glider, toad, beehive});
 }
