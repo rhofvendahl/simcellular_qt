5b9dd2f6ddf9b4d0059e392d2a86984d271d0efb
russell Hofvendahl <rhofvendahl@gmail.com>
2019-10-17 18:13:28 -0700

    partially implement grid layout

--- widget.cpp
+++ widget.cpp
@@ -1,7 +1,6 @@
 #include "widget.h"
 #include "ui_widget.h"
 
-#include <QTableWidget>
 #include <QFile>
 #include <QDebug>
 #include <QGridLayout>
@@ -10,6 +9,8 @@
 #include <QLabel>
 #include <QFontDatabase>
 #include <QHeaderView>
+#include <QSpacerItem>
+#include <QSizePolicy>
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
@@ -26,22 +27,35 @@ Widget::Widget(QWidget *parent)
     int id = QFontDatabase::addApplicationFont("open_sans_condensed_light.ttf");
     QString openSansCondensed = QFontDatabase::applicationFontFamilies(id).at(0);
 
-    simcellular = new QGridLayout(this);
+    QGridLayout *simcellularLayout = new QGridLayout(this);
 
     title = new QLabel("simcellular");
     QFont titleFont = QFont(openSansCondensed);
     titleFont.setPixelSize(60);
     title->setFont(titleFont);
     title->setStyleSheet("background-color: red;");
-    simcellular->addWidget(title, 0, 1, 4, 2);
+    simcellularLayout->addWidget(title, 0, 1, 4, 2);
 
-    board = new QTableWidget(10, 10);
-//    board->setFixedSize(600, 600);
-    board->horizontalHeader()->setDefaultSectionSize(3);
-    board->horizontalHeader()->hide();
-    board->verticalHeader()->setDefaultSectionSize(3);
-    board->verticalHeader()->hide();
-    simcellular->addWidget(board, 1, 1, 4, 2);
+    board = new QFrame();
+    board->setFixedSize(60, 120);
+    board->setStyleSheet("background-color: green;");
+//    QSizePolicy sizePolicy (QSizePolicy::Expanding, QSizePolicy::Expanding);
+//    board->setSizePolicy(sizePolicy);
+    QGridLayout *boardLayout = new QGridLayout(board);
+    boardLayout->setContentsMargins(0, 0, 0, 0);
+    boardLayout->setSpacing(0);
+    for (int row = 0; row < 20; row++) {
+        for (int column = 0; column < 20; column++) {
+            QFrame *cell = new QFrame();
+            cell->setStyleSheet("background-color: blue");
+            cell->setFixedSize(3,3);
+            boardLayout->addWidget(cell, row, column, 21, 21);
+        }
+    }
+
+    QSpacerItem *horizontalSpacer = new QSpacerItem(1, 1, QSizePolicy::Fixed, QSizePolicy::Expanding);
+    boardLayout->addItem(horizontalSpacer, 0, 20, 21, 21);
+    simcellularLayout->addWidget(board, 1, 1, 4, 2);
 
 
     about = new QLabel("about");
@@ -49,37 +63,38 @@ Widget::Widget(QWidget *parent)
     aboutFont.setPixelSize(50);
     about->setFont(aboutFont);
     about->setStyleSheet("background-color: red;");
-    simcellular->addWidget(about, 3, 1, 4, 2);
+    simcellularLayout->addWidget(about, 3, 1, 4, 2);
 
-    shape0 = new QTableWidget(1, 1);
-    shape1 = new QTableWidget(1, 1);
-    shape2 = new QTableWidget(1, 1);
-    shape3 = new QTableWidget(1, 1);
-    shape4 = new QTableWidget(1, 1);
-    shape5 = new QTableWidget(1, 1);
-    shape6 = new QTableWidget(1, 1);
-    colors = new QTableWidget(2, 2);
+    shape0 = new QFrame();
+    shape1 = new QFrame();
+    shape2 = new QFrame();
+    shape3 = new QFrame();
+    shape4 = new QFrame();
+    shape5 = new QFrame();
+    shape6 = new QFrame();
+    colors = new QFrame();
 
-    shape0->setFixedSize(63, 63);
-    shape1->setFixedSize(63, 63);
-    shape2->setFixedSize(63, 63);
-    shape3->setFixedSize(63, 63);
-    shape4->setFixedSize(63, 63);
-    shape5->setFixedSize(63, 63);
-    shape6->setFixedSize(63, 63);
-    colors->setFixedSize(63, 63);
+    QGridLayout *shape0Layout = new QGridLayout(shape0);
+    QGridLayout *shape1Layout = new QGridLayout(shape1);
+    QGridLayout *shape2Layout = new QGridLayout(shape2);
+    QGridLayout *shape3Layout = new QGridLayout(shape3);
+    QGridLayout *shape4Layout = new QGridLayout(shape4);
+    QGridLayout *shape5Layout = new QGridLayout(shape5);
+    QGridLayout *shape6Layout = new QGridLayout(shape6);
+    QGridLayout *colorsLayout = new QGridLayout(colors);
 
-    sidebar = new QVBoxLayout();
-    sidebar->addWidget(shape0);
-    sidebar->addWidget(shape1);
-    sidebar->addWidget(shape2);
-    sidebar->addWidget(shape3);
-    sidebar->addWidget(shape4);
-    sidebar->addWidget(shape5);
-    sidebar->addWidget(shape6);
-    sidebar->addWidget(colors);
+    sidebar = new QFrame();
+    QVBoxLayout *sidebarLayout = new QVBoxLayout(sidebar);
+    sidebarLayout->addWidget(shape0);
+    sidebarLayout->addWidget(shape1);
+    sidebarLayout->addWidget(shape2);
+    sidebarLayout->addWidget(shape3);
+    sidebarLayout->addWidget(shape4);
+    sidebarLayout->addWidget(shape5);
+    sidebarLayout->addWidget(shape6);
+    sidebarLayout->addWidget(colors);
 
-    simcellular->addLayout(sidebar, 1, 0, 4, 2);
+    simcellularLayout->addWidget(sidebar, 1, 0, 4, 2);
 
     stepForward = new QToolButton();
     pause = new QToolButton();
@@ -88,22 +103,23 @@ Widget::Widget(QWidget *parent)
     clear = new QToolButton();
     random = new QToolButton();
 
-    controls = new QHBoxLayout();
-    controls->addWidget(stepForward);
-    controls->addWidget(pause);
-    controls->addWidget(play);
-    controls->addWidget(fastForward);
-    controls->addWidget(clear);
-    controls->addWidget(random);
+    controls = new QFrame();
+    QHBoxLayout *controlsLayout = new QHBoxLayout(controls);
+    controlsLayout->addWidget(stepForward);
+    controlsLayout->addWidget(pause);
+    controlsLayout->addWidget(play);
+    controlsLayout->addWidget(fastForward);
+    controlsLayout->addWidget(clear);
+    controlsLayout->addWidget(random);
 
-    simcellular->addLayout(controls, 2, 1, 4, 2);
+    simcellularLayout->addWidget(controls, 2, 1, 4, 2);
 
 
 //    QVBoxLayout *sidebar = new QVBoxLayout;
 
 //    setLayout(sidebar);
 
-//    boardWidget = new QTableWidget(200, 200, this);
+//    boardWidget = new QGridLayout(200, 200, this);
 }
 
 Widget::~Widget()
--- widget.h
+++ widget.h
@@ -2,10 +2,12 @@
 #define WIDGET_H
 
 #include <QtWidgets/QWidget>
-#include <QTableWidget>
-#include <QLayout>
+#include <QGridLayout>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
 #include <QLabel>
 #include <QToolButton>
+#include <QFrame>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Widget; }
@@ -22,27 +24,29 @@ public:
 private:
     Ui::Widget *ui;
 
-    QGridLayout *simcellular;
     QLabel *title;
-    QTableWidget *board;
-    QLabel *about;
 
-    QVBoxLayout *sidebar;
-    QTableWidget *shape0;
-    QTableWidget *shape1;
-    QTableWidget *shape2;
-    QTableWidget *shape3;
-    QTableWidget *shape4;
-    QTableWidget *shape5;
-    QTableWidget *shape6;
-    QTableWidget *colors;
+    QFrame *sidebar;
+    QFrame *shape0;
+    QFrame *shape1;
+    QFrame *shape2;
+    QFrame *shape3;
+    QFrame *shape4;
+    QFrame *shape5;
+    QFrame *shape6;
+    QFrame *colors;
 
-    QHBoxLayout *controls;
+    QFrame *board;
+
+    QFrame *controls;
     QToolButton *stepForward;
     QToolButton *pause;
     QToolButton *play;
     QToolButton *fastForward;
     QToolButton *clear;
     QToolButton *random;
+
+    QLabel *about;
+
 };
 #endif // WIDGET_H
