cea6eab3947be2844d6ba131c840eb610fe73b4b
russell Hofvendahl <rhofvendahl@gmail.com>
2019-10-18 20:51:32 -0700

    resolve cell.h

--- cell.h
+++ cell.h
@@ -1,3 +1,29 @@
+//#ifndef CELL_H
+//#define CELL_H
+
+//#include "color.h"
+
+//#include <QList>
+
+//class Cell
+//{
+//public:
+//    static bool areSame(Cell cell1, Cell cell2);
+//    static Color getAverageColor(QList<Cell> cells);
+
+//    Cell(int rowParam, int columnParam);
+//    bool state;
+//    bool nextState;
+//    int row; // commented out in original - vestigial?
+//    int column; // commented out in original - vestigial?
+//    Color color;
+//    Color nextColor;
+//    QList<Cell> neighbors;
+//    QList<Cell> getLiveNeighbors();
+//};
+
+//#endif // CELL_H
+
 #ifndef CELL_H
 #define CELL_H
 
@@ -8,18 +34,23 @@
 class Cell
 {
 public:
-    static bool areSame(Cell cell1, Cell cell2);
-    static Color getAverageColor(QList<Cell> cells);
+    static bool areSame(Cell *cell1, Cell *cell2);
 
     Cell(int rowParam, int columnParam);
     bool state;
     bool nextState;
     int row; // commented out in original - vestigial?
     int column; // commented out in original - vestigial?
-    Color color;
-    Color nextColor;
-    QList<Cell> neighbors;
-    QList<Cell> getLiveNeighbors();
+    Color *color;
+    Color *nextColor;
+    QList<Cell*> neighbors;
+    QList<Cell*> getLiveNeighbors();
+    void determineNext();
+    void transition();
+    void setNext(bool state, Color *colorParameter=nullptr);
+    void setNextRandom();
+    Color  *getAverageColor(QList<Cell*> liveNeighbors);
+
 };
 
 #endif // CELL_H
